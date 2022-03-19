b6ae51e2e400e340ae0f3bab868ae9f9808c9f5a
viniciushetzel <viniciuscarbonario@gmail.com>
2022-03-19 19:34:35 -0400

    Criado ProjetoRelogio, Adicionado arquivo Relogio.java, AppRelogio.java

---
+++ aula02/ProjetoRelogio/src/Relogio/AppRelogio.java
@@ -0,0 +1,9 @@
+package Relogio;
+
+public class AppRelogio {
+	public static void main(String[] args) {
+		Relogio relogio1 = new Relogio(2, 12, 40);
+
+		System.out.println(relogio1.getHoraAtual());
+	}
+}
