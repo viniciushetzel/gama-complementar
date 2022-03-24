d3001fb7ea0f5aa402fc2bcfbf3a2e1f41c5f927
viniciushetzel <viniciuscarbonario@gmail.com>
2022-03-24 15:00:55 -0400

    Criado package controller

---
+++ desafio-pratico/sistema-controle-de-contas/src/controller/GerenciaContas.java
@@ -0,0 +1,46 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.Conta;
+
+public class GerenciaContas {
+	private ArrayList<Conta> contas;
+
+	public GerenciaContas() {
+		contas = new ArrayList<>();
+	}
+
+	public void adicionar(Conta c) {
+		contas.add(c);
+	}
+
+	public String exibir(int numeroConta) {
+		for (int i = 0; i < contas.size(); i++) {
+			if (contas.get(i).getNumeroConta() == numeroConta) {
+				return contas.get(i).toString();
+			}
+		}
+		return "Desculpe, a conta informada não está cadastrada no sistema.";
+	}
+
+	private int posicao(int numeroConta) {
+		for (int i = 0; i < contas.size(); i++) {
+			if (contas.get(i).getNumeroConta() == numeroConta) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public boolean remover(int numeroConta) {
+		int posicao = posicao(numeroConta);
+		if (posicao >= 0) {
+			if (contas.remove(posicao) != null) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
